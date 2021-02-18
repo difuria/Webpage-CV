835031a82fa24976c4fa05cb4673076f949ba29e
difuria <69213222+difuria@users.noreply.github.com>
2021-02-18 21:42:09 +0000

    Make experience section dynamically created

--- server.py
+++ server.py
@@ -33,10 +33,13 @@ def favicon():
 def my_home():
     return render_template('index.html')
 
-def get_duration(start_date):
+def get_duration(start_date, end_date=None):
     duration_string = ''
-    current_date = datetime.now()
-    duration_time = relativedelta.relativedelta(current_date, start_date) 
+
+    if not end_date:
+        end_date = datetime.now()
+
+    duration_time = relativedelta.relativedelta(end_date, start_date) 
     if duration_time.years > 0:
         duration_string += f'{duration_time.years} year'
         if duration_time.years > 1:
@@ -62,7 +65,101 @@ def get_duration(start_date):
 @app.route('/<string:html_page>')
 def page(html_page="index.html"):
     if html_page == "Experience and Education.html":
-        return render_template(html_page, company_duration=get_duration(datetime(2014,11,24)), role_duration=get_duration(datetime(2021,1,1)))
+        with open("Experience_and_Education.json") as json_file:
+            data = json.load(json_file)
+        
+        experience = "                  <section>"
+        for company in data["Experience"]:
+            company_start_date = datetime.now()
+            company_end_date = None
+            roles = ""
+            for role in company["Roles"]:
+                role_start_date = datetime.strptime(role["Start Date"], "%d/%m/%Y")
+                s_d = role_start_date.strftime("%b %Y")
+                if role["End Date"]:
+                    role_end_date = datetime.strptime(role["End Date"], "%d/%m/%Y")
+                    e_d = role_end_date.strftime("%b %Y")
+                else:
+                    role_end_date = datetime.now()
+                    e_d = "Present"
+
+                if company_start_date > role_start_date:
+                    company_start_date = role_start_date
+                if not company_end_date or company_end_date < role_end_date:
+                    company_end_date = role_end_date
+
+                roles += f"""                    <div class="row">
+                      <h4>{role["Role"]}</h4>
+                    </div>
+                    <div class="row">
+                      {s_d} - {e_d} ({get_duration(role_start_date, role_end_date)})
+                    </div>"""
+
+                if role["Location"]:
+                    roles += f"""                    <div class="row">
+                      {role["Location"]}
+                    </div>\n"""
+                if role["Description"]:
+                    role["Description"] = role["Description"].replace("\n", "\n<br>")
+                    roles += f"""                    <div class="row">
+                      <br>{role["Description"]}
+                    </div>\n"""
+
+            experience += f"""                    <div class="row">
+                      <div class="column" style="white-space:normal">
+                        <a href="{company["url"]}"><img src="static/assets/images/Logos/{company["Logo"]}" alt="" class="img-responsive" style="width:50px;height:50px" align="left"/></a>
+                      </div>
+                      <div class="column" style="white-space:normal">
+                          <h4>Fidessa</h4>
+                          {get_duration(company_start_date, company_end_date)}
+                      </div>
+                    </div>\n{roles}"""
+        experience += "                  </section>"
+
+        education = "                  <section>"
+        for school in data["Education"]:
+            start = datetime.strptime(school["Start Date"], "%m/%Y")
+            end   = datetime.strptime(school["End Date"], "%m/%Y")
+            duration   = get_duration(start, end)
+            # Display date in short easy to ready form such as Jun 2009
+            s_d = start.strftime("%b %Y")
+            e_d = end.strftime("%b %Y")
+            education += f"""                    <div class="row">
+                      <div class="column" style="white-space:normal">
+                        <a href="{school["url"]}"><img src="static/assets/images/Logos/{school["Logo"]}" alt="" class="img-responsive" style="width:50px;height:50px" align="left"/></a>
+                      </div>
+                      <div class="column" style="white-space:normal">
+                          <h4>{school["School"]}</h4>
+                          {duration}                       
+                      </div>
+                    </div>"""
+
+            # Don't have a course in secondary school
+            if school["Course"]:
+                education += f"""                    <div class="row">
+                      <h4>{school["Course"]}</h4>
+                    </div>"""
+
+            grades = school["Grades"].replace('\n', '\n<br>')
+            education += f"""                    <div class="row">
+                      {s_d} – {e_d}
+                    </div>
+                    <div class="row">
+                      <p>{grades}</p>
+                    </div>\n"""
+        education += "                  </section>"
+
+        technical_skills = ""
+        for skill in data["Technical Skills"]:
+            technical_skills += f"""                  <div class="row">
+                    <div class="col-xs-3 col-md-2">
+                      <p>{skill["Skill"]}</p>
+                    </div>
+                    <div class="col-xs-9 col-md-10">
+                      <p>{skill["Description"]}</p>
+                    </div>
+                  </div>"""
+        return render_template(html_page, company_duration=get_duration(datetime(2014,11,24)), role_duration=get_duration(datetime(2021,1,1)), tech_skills=technical_skills, edu=education, exp=experience)
     else:
         return render_template(html_page)
 
